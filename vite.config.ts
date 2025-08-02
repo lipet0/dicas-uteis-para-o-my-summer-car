c992e498e5db6d71a6f2a21b435fcc1f1b383208
lipet0 <lipetorressilva06@gmail.com>
2025-08-02 00:47:30 -0300

    deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/dicas-uteis-para-o-my-summer-car/",  // <<< ADICIONE ESSA LINHA
   server: {
     host: "::",
     port: 8080,
